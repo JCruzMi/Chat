c8a018db16395215cfbce10058696465439893ee
JCruzMi <47216265+JCruzMi@users.noreply.github.com>
2019-12-04 22:13:37 -0500

    Finally

--- chatbeta/src/main.js
+++ chatbeta/src/main.js
@@ -1,15 +1,18 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
-import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
